f99789be194558ba68d863ca69dfbc1f4390a149
--unset <--unset>
2025-02-10 08:31:25 +0100

    megadott kod es instrukciok

---
+++ WebAPIgyakorlas/Models/LibraryContent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIgyakorlas.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
